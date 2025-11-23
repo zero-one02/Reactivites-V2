3772df402241517e6cb2aa4f33900d684f442ad7
chengwei.sun <sandtek@CHENGWEI-SUN>
2025-11-23 22:38:41 +0800

    End of  section 2

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var service = scope.ServiceProvider;
+
+try
+{
+    var context = service.GetService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = service.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error accured during migration.");
+}
+
+app.Run();
